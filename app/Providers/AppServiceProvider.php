486a2a468052400f89d49ebdf7f78cd03ffd1a14
RenV564 <renatavero564@gmail.com>
2025-03-07 16:52:35 +0700

    second commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,11 +3,14 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Pagination\Paginator;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
+     * 
+     * @return void
      */
     public function register(): void
     {
@@ -16,9 +19,11 @@ class AppServiceProvider extends ServiceProvider
 
     /**
      * Bootstrap any application services.
+     * 
+     * @return void
      */
     public function boot(): void
     {
-        //
+        Paginator::useBootstrap();
     }
 }
